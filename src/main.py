397b39df42f47348b2521809bd3797b0ca3545fd
asif256000 <asif256000@gmail.com>
2023-12-19 11:51:33 +0530

    Route Updates

---
+++ src/main.py
@@ -0,0 +1,21 @@
+# Author: Asif Iqbal Rahaman
+# Start Date: 2023-12-16T12:30:00+05:30
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("base.html", {"request": request})
+
+
+@app.get("/profile", response_class=HTMLResponse)
+def read_profile(request: Request):
+    return templates.TemplateResponse("profile.html", {"request": request, "user": "Asif"})
