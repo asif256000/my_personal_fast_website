27a7bae4d4cda7dcd85036ced92d2bbc1aa5c0f9
asif256000 <asif256000@gmail.com>
2023-12-26 17:42:51 +0530

    Dark Mode Debugging

--- src/main.py
+++ src/main.py
@@ -11,11 +11,11 @@ templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+@app.get("/profile", name="profile", response_class=HTMLResponse)
+def read_profile(request: Request):
+    return templates.TemplateResponse("profile.html", {"request": request})
+
+
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-
-@app.get("/profile", response_class=HTMLResponse)
-def read_profile(request: Request):
-    return templates.TemplateResponse("profile.html", {"request": request})
--- static/dark-mode.js
+++ static/dark-mode.js
@@ -1,5 +1,8 @@
 const darkModeToggle = document.getElementById("dark-mode-toggle");
+let toggleText = document.getElementById("toggle-text");
 const body = document.body;
+const dark_mode_text = "Welcome to the dark side!";
+const light_mode_text = "Come to the dark side...";
 
 // Check if user has a preferred color scheme and set initial mode accordingly
 if (
@@ -7,9 +10,20 @@ if (
   window.matchMedia("(prefers-color-scheme: dark)").matches
 ) {
   body.classList.add("dark-mode");
+  darkModeToggle.checked = true;
+  toggleText.innerHTML = dark_mode_text;
+} else {
+  body.classList.remove("dark-mode");
+  darkModeToggle.checked = false;
+  toggleText.innerHTML = light_mode_text;
 }
 
 // Toggle dark mode on button click
 darkModeToggle.addEventListener("change", () => {
   body.classList.toggle("dark-mode");
+  if (body.classList.contains("dark-mode")) {
+    toggleText.innerHTML = dark_mode_text;
+  } else {
+    toggleText.innerHTML = light_mode_text;
+  }
 });
