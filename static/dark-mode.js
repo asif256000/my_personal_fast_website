f98b3c1b20b933519cb38e79f91e4c53dc35afaf
asif256000 <asif256000@gmail.com>
2024-01-06 13:57:58 +0530

    Consistent Dark Mode Fix

--- static/dark-mode.js
+++ static/dark-mode.js
@@ -1,11 +1,14 @@
 let darkMode = localStorage.getItem("darkMode");
+const systemDarkMode =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-const darkModeToggle = document.querySelector("#dark-mode-toggle");
-// const darkModeToggle = document.getElementById("dark-mode-toggle");
-let toggleText = document.getElementById("toggle-text");
+// const darkModeToggle = document.querySelector("#dark-mode-toggle");
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+const toggleText = document.getElementById("toggle-text");
 const body = document.body;
-const dark_mode_text = "Welcome to the dark side!";
-const light_mode_text = "Come to the dark side...";
+const dark_mode_text = "Welcome to the dark side!&nbsp;&nbsp;&nbsp;";
+const light_mode_text = "Come to the dark side...&nbsp;&nbsp;&nbsp;";
 
 const enableDarkMode = () => {
   // 1. Add the class to the body
@@ -20,30 +23,26 @@ const disableDarkMode = () => {
   // 1. Remove the class from the body
   body.classList.remove("dark-mode");
   // 2. Update darkMode in localStorage
-  localStorage.setItem("darkMode", null);
+  localStorage.setItem("darkMode", "disabled");
   darkModeToggle.checked = false;
   toggleText.innerHTML = light_mode_text;
 };
 
 // Check if user has a preferred color scheme and set initial mode accordingly
-if (
-  (window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches) ||
-  darkMode
-) {
+if (darkMode === "enabled") {
   enableDarkMode();
-} else {
+} else if (darkMode === "disabled") {
   disableDarkMode();
+} else if (systemDarkMode) {
+  enableDarkMode();
 }
 
 darkModeToggle.addEventListener("click", () => {
   // get the current value of the "darkMode" item in the localStorage
   darkMode = localStorage.getItem("darkMode");
 
-  // if darkMode is enabled, disable it
-  if (darkMode) {
+  if (darkMode === "enabled") {
     disableDarkMode();
-    // if darkMode is not enabled, enable it
   } else {
     enableDarkMode();
   }
