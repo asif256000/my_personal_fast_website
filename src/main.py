653c5bd4d18c3fbc4634df569f66b71707c689b0
asif256000 <asif256000@gmail.com>
2024-01-21 15:25:04 -0500

    Run server code added

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 # Author: Asif Iqbal Rahaman
 # Start Date: 2023-12-16T12:30:00+05:30
 
+# import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 from src.web import routes
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
