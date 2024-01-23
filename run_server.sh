aebd5caedbbb05552b93158a7b9d4af322d96d6e
asif256000 <asif256000@gmail.com>
2024-01-23 12:45:35 -0500

    Update uvicorn command to get all logs

--- run_server.sh
+++ run_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run FastAPI application in the background and redirect outputs to logs
-nohup python3 -m uvicorn src.main:app --log-level warning >> logs/fastapi.log 2>&1 &
+nohup python3 -m uvicorn src.main:app >> logs/fastapi.log 2>&1 &
 
 # Set up log rotation
 logrotate -s logs/logrotate.status -v logs/fastapi_logrotate.conf
