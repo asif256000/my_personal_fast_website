e29a9fae0633e90b789c8c655d0284e66bfe81f2
asif256000 <asif256000@gmail.com>
2024-01-23 01:43:13 -0500

    Test Modules for data and routes

--- test/unit/web/test_routes.py
+++ test/unit/web/test_routes.py
@@ -11,4 +11,26 @@ def test_read_main():
     assert response.status_code == 200
 
 
-# Add more tests for other routes in routes.py
+def test_read_overview():
+    response = client.get("/overview")
+    assert response.status_code == 200
+
+
+def test_read_experience_education():
+    response = client.get("/experience_education")
+    assert response.status_code == 200
+
+
+def test_read_projects_certificates():
+    response = client.get("/projects_certificates")
+    assert response.status_code == 200
+
+
+def test_read_awards_interests():
+    response = client.get("/awards_interests")
+    assert response.status_code == 200
+
+
+def test_read_resume():
+    response = client.get("/resume")
+    assert response.status_code == 200
