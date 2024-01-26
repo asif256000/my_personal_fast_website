251b484e96602e7c96d7799654d9ded8b031cf36
asif256000 <asif256000@gmail.com>
2024-01-26 00:37:35 -0500

    Manual database update script

--- src/manual_db_update.py
+++ src/manual_db_update.py
@@ -1,55 +1,75 @@
 from pprint import pprint
 
 from data import db_ops
-from web.routes import get_db
+from data.db_init import get_db
 
 
 def manual_update_db_entries():
     db = next(get_db())
-    table = "education"
-    filter_data = {"institution": "Virginia Tech", "fk_user": 1}
-    updated_data = {
-        "major": "Computer Science and Applications",
-        "gpa": "3.85 out of 4",
-        "courses": "Information Visualization, Fundamentals of Information Security, Applications of Machine Learning, Theory of Algorithms, Data Analysis, Computer Vision, Natural Language Processing, AI for Software Delivery",
-    }
-    filtered_data = db_ops.filter_data_from_table(db, table, filter_data)
-    print(f"Updating data for {table=} for {filter_data=}. {updated_data=}")
-    db_ops.update_data_with_id(
-        db, table_name="education", lookup_id=filtered_data[0].id, data={"degree": "Master of Engineering"}
-    )
+    table = "experience"
+    # filter_data = {"institution": "Virginia Tech", "fk_user": 1}
+    updated_data = {}
+    filtered_data = db_ops.filter_data_with_id(db, table, lookup_id=1)
+    print(f"Updating data for {table=} for id={filtered_data.id}. {updated_data=}")
+    db_ops.update_data_with_id(db, table_name=table, lookup_id=filtered_data.id, data=updated_data)
     db.close()
 
     db = next(get_db())
-    filter_data = {"institution": "Vellore Institute of Technology (VIT), Vellore", "fk_user": 1}
-    updated_data = {"major": "Computer Science and Engineering"}
-    filtered_data = db_ops.filter_data_from_table(db, table, filter_data)
-    print(f"Updating data for {table=} for {filter_data=}. {updated_data=}")
-    db_ops.update_data_with_id(
-        db, table_name="education", lookup_id=filtered_data[0].id, data={"degree": "Master of Engineering"}
-    )
+    table = "experience"
+    # filter_data = {"institution": "Vellore Institute of Technology (VIT), Vellore", "fk_user": 1}
+    updated_data = {}
+    filtered_data = db_ops.filter_data_with_id(db, table, lookup_id=2)
+    print(f"Updating data for {table=} for id={filtered_data.id}. {updated_data=}")
+    db_ops.update_data_with_id(db, table_name=table, lookup_id=filtered_data.id, data=updated_data)
+    db.close()
+
+    db = next(get_db())
+    table = "experience"
+    # filter_data = {"institution": "Vellore Institute of Technology (VIT), Vellore", "fk_user": 1}
+    updated_data = {}
+    filtered_data = db_ops.filter_data_with_id(db, table, lookup_id=3)
+    print(f"Updating data for {table=} for id={filtered_data.id}. {updated_data=}")
+    db_ops.update_data_with_id(db, table_name=table, lookup_id=filtered_data.id, data=updated_data)
     db.close()
 
 
 def manual_read_db_entries():
     db = next(get_db())
-    for entry in db_ops.read_data_from_table(db, "basic_info"):
+    table_name = "basic_info"
+    for entry in db_ops.read_data_from_table(db, table_name):
+        print(f"{table_name=}")
         pprint(vars(entry))
-    for entry in db_ops.read_data_from_table(db, "education"):
+    table_name = "education"
+    for entry in db_ops.read_data_from_table(db, table_name):
+        print(f"{table_name=}")
         pprint(vars(entry))
-    for entry in db_ops.read_data_from_table(db, "experience"):
+    table_name = "experience"
+    for entry in db_ops.read_data_from_table(db, table_name):
+        print(f"{table_name=}")
         pprint(vars(entry))
-    for entry in db_ops.read_data_from_table(db, "skills"):
+    table_name = "skills"
+    for entry in db_ops.read_data_from_table(db, table_name):
+        print(f"{table_name=}")
         pprint(vars(entry))
-    for entry in db_ops.read_data_from_table(db, "projects"):
+    table_name = "projects"
+    for entry in db_ops.read_data_from_table(db, table_name):
+        print(f"{table_name=}")
         pprint(vars(entry))
-    for entry in db_ops.read_data_from_table(db, "certifications"):
+    table_name = "certifications"
+    for entry in db_ops.read_data_from_table(db, table_name):
+        print(f"{table_name=}")
         pprint(vars(entry))
-    for entry in db_ops.read_data_from_table(db, "honors_and_awards"):
+    table_name = "honors_and_awards"
+    for entry in db_ops.read_data_from_table(db, table_name):
+        print(f"{table_name=}")
         pprint(vars(entry))
-    for entry in db_ops.read_data_from_table(db, "interests"):
+    table_name = "interests"
+    for entry in db_ops.read_data_from_table(db, table_name):
+        print(f"{table_name=}")
         pprint(vars(entry))
-    for entry in db_ops.read_data_from_table(db, "resume"):
+    table_name = "resume"
+    for entry in db_ops.read_data_from_table(db, table_name):
+        print(f"{table_name=}")
         pprint(vars(entry))
     db.close()
 
